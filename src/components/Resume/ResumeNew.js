97318c4b17605b5e4b50b968c9a72bcd91be8255
Disha <dishavig1@gmail.com>
2023-08-19 16:12:30 +0530

    updated Resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/resume-august-2023.pdf";
+import pdf from "../../Assets/resume_august_2023_2.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
